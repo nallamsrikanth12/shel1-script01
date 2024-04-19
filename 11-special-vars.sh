c338c29fca7866ce900dc56d9ff68f249ff6301d
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-20 02:46:32 +0630

    new

--- 11-special-vars.sh
+++ 11-special-vars.sh
@@ -6,7 +6,5 @@ echo "script Name: $0"
 echo "current working directory :$PWD"
 echo "home directory of current user: $HOME"
 echo "process id of the current shell script: $$"
-
+sleep 60 $
 echo "process id of last background command: $!"
-
-slepp 20 &
