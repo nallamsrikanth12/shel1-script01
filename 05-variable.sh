6a0ac82e536a2d5bb2b15a831123c4ce0c90a2ef
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-19 20:07:22 +0630

    new

---
+++ 05-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=srikanth
+PERSON2=ashok
+
+echo "$PERSON1: hii $PERSON2 how are u?"
+echo "$PERSON2:iam good how about u"
+echo "$PERSON1 and $PERSON2 we can go goa trip this summer hoildays"
