b15312676fd26d75a32e742a4a1c8a3de94d7912
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-22 11:58:06 +0630

    function

---
+++ 15-function.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo "$2 is failure"
+else
+    echo "$2 is sucuess"
+fi
+}
+
+
+if [ $USERID -ne 0 ]
+then
+    echo"u are not super user"
+else
+    echo "u are a super user"
+fi
+
+
+dnf install mysql -y
+VALIDATE $? "installing mysql sucuessfully"
+
+
+dnf install git -y
+VALIDATE $? "installing git sucuessfully"
+
+
+
+dnf install java -y
+VALIDATE $? "installing java sucuessfully"
+
+
+
