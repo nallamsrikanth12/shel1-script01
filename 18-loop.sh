f6e0eebf2eb99acde4508eac6f3eff40da1ba857
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-22 21:58:58 +0630

    installation

--- 18-loop.sh
+++ 18-loop.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+P="\e[34m"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+  echo -e "$2 $R failure $N"
+elso
+  echo  -e "$2  $G success"  
+}
 
 if [ $USERID -ne 0 ]
 then 
-    echo "you are not a super user"
+  echo "you are not a super user"
+
 else
-    echo "you are super user"
+  echo "you are super user"
 fi
 
 for i in $@
@@ -17,6 +33,7 @@ do
   then
     echo "$i already installed ...skkippig"
   else
-    echo "$1 not installed..need to install"  
+    dnf -e install $i -y 
+    VALIDATE $? "installating"
   fi   
 done 
