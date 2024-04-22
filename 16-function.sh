18ce2bfe6d15e6edb3cb67c3deb1ee3140e9c086
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-22 14:51:35 +0630

    logfile

--- 16-function.sh
+++ 16-function.sh
@@ -1,4 +1,4 @@
-USERID=$(id-u)
+USERID=$(id -u)
 TIMESTAND=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAND.log
@@ -13,7 +13,7 @@ fi
 }
 
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
     echo "you are not super user"
 else
