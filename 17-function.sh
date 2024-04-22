7fc6d0216a741e8c968cf628af64912b8811bf23
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-22 15:53:53 +0630

    logfile

---
+++ 17-function.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="/e[31m"
+G="/e[32m"
+N="/e[0m"
+
+VALIDATE(){
+if [ $? -ne 0 ]
+then
+    echo -e "$2...$R FAILURE $N"
+else 
+    echo -e "$2 ...$G success $N"
+fi
+}
+
+if [ $USERID -ne 0]
+then
+    echo "you are not super user"
+else
+    echo "you are super user"
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "installed"
+
+dnf install git - &>>$LOGFILE
+VALIDATE $? "installed"
+
+dnf  install docker -y &>>$LOGFILE
+VALIDATE $? "installed" 
